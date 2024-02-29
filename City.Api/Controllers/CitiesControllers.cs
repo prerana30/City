2fe992299959bee1b13a82c3355fcf016088cc3c
phenol2002 <86253756+prerana30@users.noreply.github.com>
2024-02-29 14:47:46 +0545

    Added a class controller - added Route("api/cities") in a code as we are building controller by ourself.

---
+++ City.Api/Controllers/CitiesControllers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace City.Controller
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesControllers : ControllerBase
+
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                 new List<object>
+                 {
+                 new { id=1, Name="New York City"},
+                 new{ id=2, Name="Antwerp"} // added few lines.
+                 });
+        }
+    }
+}
